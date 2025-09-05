4b0f55ef988a942e9bee033a8bba7cce2998fc24
SophieZ11 <ffzhang09@outlook.com>
2025-09-05 10:10:04 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -9,9 +9,9 @@ export default siteConfig({
   rootNotionSpaceId: null,
 
   // basic site info (required)
-  name: 'Next.js Notion Starter Kit',
-  domain: 'nextjs-notion-starter-kit.transitivebullsh.it',
-  author: 'Travis Fischer',
+  name: 'Fengfei Zhang Portfolio',
+  domain: 'nextjs-notion-starter-kit.transitivebulls.it',
+  author: 'Fengfei Zhang',
 
   // open graph metadata (optional)
   description: 'Example Next.js Notion Starter Kit Site',
